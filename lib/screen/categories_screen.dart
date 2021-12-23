7e461946c1ac7477ee41f514d2d5c275382b8c75
Abdul_Rohman <rokhman12abdul@gmail.com>
2021-12-23 15:26:52 +0700

    add Pop Up for Create Categories

--- lib/screen/categories_screen.dart
+++ lib/screen/categories_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_screen.dart';
+
 class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
@@ -8,8 +10,68 @@ class CategoriesScreen extends StatefulWidget {
 }
 
 class _CategoriesScreenState extends State<CategoriesScreen> {
+  _showFormDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (param) {
+          return AlertDialog(
+            actions: [
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Cancel"),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {},
+                child: Text("Save"),
+              ),
+            ],
+            title: Text("Categories Form"),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Write a Category",
+                      labelText: "Category",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Welcome to Categories Screen");
+    return Scaffold(
+      appBar: AppBar(
+        leading: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => HomeScreen()));
+          },
+          child: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+        ),
+        title: Text("Categories"),
+      ),
+      body: Center(
+        child: Text("Welcome to Categories Screen"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showFormDialog(context);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
